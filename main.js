2b4a4a6a8b54e6e383b83c102b95d9899efd6c2c
RahidDev <rkhan14702@gmail.com>
2021-11-10 14:11:46 +0000

    Enter

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let dataClass = [];
 
-
+/// WritePointer - When you reach end of buffer set point to 0. A ring od ata almost like a loop of tape. 
+/// Store 1000 values when it hits 1,000 reset to 0.
+/// Read data from write pointer - 500 (may take u to 0) and read the data behind the writepointer.
+///
 
 if (window.DeviceOrientationEvent) 
 {
