ef66343a3005fb6fae9007a80ba1a3a62c8708a7
RahidDev <rkhan14702@gmail.com>
2021-11-11 11:40:23 +0000

    Enter

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ let dataClass = [];
 /// WritePointer - When you reach end of buffer set point to 0. A ring od ata almost like a loop of tape. 
 /// Store 1000 values when it hits 1,000 reset to 0.
 /// Read data from write pointer - 500 (may take u to 0) and read the data behind the writepointer.
-///
 
 if (window.DeviceOrientationEvent) 
 {
     window.addEventListener("deviceorientation", function (event) 
     { 
-        let para = document.createElement("p");
-        document.body.appendChild(para);
-        para.innerText = 'Alpha:' + event.alpha + ', Beta:' + event.beta + ',Gamma: ' + event.gamma;
+     //   let para = document.createElement("p");
+    //    document.body.appendChild(para);
+     //   para.innerText = 'Alpha:' + event.alpha + ', Beta:' + event.beta + ',Gamma: ' + event.gamma;
+
     }, true);
 
 } 
@@ -19,6 +19,9 @@ else if (window.DeviceotiMonEvent)
 {
     window.addEventListener('devicemotion', function (event)
     { 
+        let para = document.createElement("p");
+        document.body.appendChild(para);
+        para.innerText = 'X:' + event.x + ', Y:' + event.y + ',Z: ' + event.z;
     }, true);
 } 
 else 
