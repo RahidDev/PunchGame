ee727b4c58354b786b1dd0dac693ae3ea0bb7532
RahidDev <rkhan14702@gmail.com>
2021-11-11 12:20:39 +0000

    Enter

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ else if (window.DeviceotiMonEvent)
     { 
         let para2 = document.createElement("p");
         document.body.appendChild(para2);
-        para2.innerText = 'X:' + event.x + ', Y:' + event.y + ',Z: ' + event.z;
+        para2.innerText = 'X:' + event.X + ', Y:' + event.Y + ',Z: ' + event.Z;
     }, true);
 } 
 else 
